b4190b7fa0761b227da85238734a937ce8cdf30b
Alnis Smidchens <alnis@mega-gherkin.Home>
2021-04-27 00:37:41 -0700

    bees come from hive and can get grumpy

--- entities/bee/bee_manager.gd
+++ entities/bee/bee_manager.gd
@@ -1,37 +1,66 @@
 extends Node2D
 
+enum State {IDLE, DRAWING, FOLLOWING, RETURNING, BACKUP}
 
-onready var current_line2D := Line2D.new()
+var current_line2D := Line2D.new()
 var curve2d : Curve2D
 var last_position : Vector2
 export var speed = 200
-var started = false #stops the pathfollow setting off without a path
-var drawing = false
+
+var state = State.IDLE
+var time = 0.0
+
+var in_flower = false
+var grumpy = false
 
 var flowers := []
 
 func _ready():
 	add_child(current_line2D)
+	print("initialized")
 
 func _process(delta):
-	if $Path2D/BeeLocator.unit_offset < 1 && started:
+	time += delta
+	
+	if $Path2D/BeeLocator.unit_offset < 1 && state == State.FOLLOWING:
 		$Path2D/BeeLocator.offset += speed * delta
+	
+	if $Path2D/BeeLocator.unit_offset > 0 && state == State.BACKUP:
+		$Path2D/BeeLocator.offset -= speed * delta
+	
+	if state == State.RETURNING:
+		$Path2D/BeeLocator/AnimatedSprite.scale -= Vector2(delta, delta) * .5
+		var rotation = $Path2D/BeeLocator.rotation
+		$Path2D/BeeLocator.translate(Vector2(cos(rotation), sin(rotation)) * delta * speed)
+		if $Path2D/BeeLocator/AnimatedSprite.scale.x < 0:
+			queue_free() 
 	
 	last_position = $Path2D/BeeLocator.position
 	
-	if Input.is_action_just_pressed("draw") and get_local_mouse_position().distance_to($Path2D/BeeLocator.global_position) < 40.0:
+	if (Input.is_action_just_pressed("draw")
+			and get_local_mouse_position().distance_to($Path2D/BeeLocator.global_position) < 40.0
+			and (state == State.IDLE or state == State.FOLLOWING)):
 		_start_drawing()
-	if Input.is_action_pressed("draw") and drawing:
+	if Input.is_action_pressed("draw") and state == State.DRAWING:
 		_continue_drawing()
-	if Input.is_action_just_released("draw") and drawing:
+	if Input.is_action_just_released("draw") and state == State.DRAWING:
 		_finish_drawing()
 	
+	if state == State.FOLLOWING and $Path2D/BeeLocator.unit_offset == 1.0:
+		state = State.IDLE
 	
+	if (state == State.IDLE or state == State.DRAWING) and in_flower:
+		$Path2D/BeeLocator/AnimatedSprite.playing = false
+		$Path2D/BeeLocator/AnimatedSprite.frame = 0
+	else:
+		$Path2D/BeeLocator/AnimatedSprite.playing = true
+	
+	if grumpy:
+		$Path2D/BeeLocator/Ball.show()
 
 func _start_drawing():
-	started = false
 	curve2d = null
-	drawing = true
+	state = State.DRAWING
 	current_line2D.points = PoolVector2Array()
 	current_line2D.show()
 
@@ -55,15 +84,21 @@ func _finish_drawing():
 	$Path2D.curve = curve2d
 	$Path2D/BeeLocator.offset = 0
 	current_line2D.hide()
-	drawing = false
-	started = true
+	state = State.FOLLOWING
+	$Path2D/BeeLocator/AnimatedSprite.show()
 
 func _on_BeeCollisionArea_area_entered(area):
 	print("ran into other bee")
+	if state == State.FOLLOWING:
+		state = State.BACKUP
+	$BackupTimer.start()
+	grumpy = true
+	flowers = []
 
 
 func _on_BeeCollectionArea_area_entered(area):
 	print("collection area entered")
+	in_flower = true
 	if !flowers.has(area):
 		flowers.append(area)
 		print("visited new flower")
@@ -71,8 +106,22 @@ func _on_BeeCollectionArea_area_entered(area):
 		print("visited old flower")
 
 
+func _on_BeeCollectionArea_area_exited(area):
+	in_flower = false
+
+
 func _on_BeeHiveReturnArea_area_entered(area):
 	print("hive entered")
+	
+	# if the bee was just added, ignore the first extra signal
+	if time < 0.1:
+		return
+	
 	if area.has_method("register_delivery"):
 		area.register_delivery(flowers.size())
-	flowers = []
+		flowers = []
+		state = State.RETURNING
+
+
+func _on_BackupTimer_timeout():
+	state = State.IDLE
--- environment/hive/hive.gd
+++ environment/hive/hive.gd
@@ -6,6 +6,8 @@ var pollen = 0.0
 var time = 0.0
 var last_time = 0.0
 
+const BEE = preload("res://entities/bee/bee_manager.tscn")
+
 func _process(delta):
 	honey -= delta * 0.5
 	time += delta
@@ -13,9 +15,19 @@ func _process(delta):
 		print("honey: ", honey)
 		print("pollen: ", pollen)
 		last_time = time
+	
+	if (Input.is_action_just_pressed("draw")
+			and get_local_mouse_position().distance_to(global_position) < 80.0):
+		var bee = BEE.instance()
+		bee.get_node("Path2D/BeeLocator/AnimatedSprite").hide()
+		bee._start_drawing()
+		bee.current_line2D.add_point(get_local_mouse_position())
+		add_child(bee)
 
 func register_delivery(flowers: int) -> void:
 	honey += flowers
 	pollen += flowers
 	print("honey: ", honey)
 	print("pollen: ", pollen)
+
+
