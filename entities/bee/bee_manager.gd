1f51258cd8b0b143daf3109c23ccd0a7dc0d6c6a
Alnis Smidchens <alnis@mega-gherkin.Home>
2021-04-26 21:46:59 -0700

    refactor drawing code

--- entities/bee/bee_manager.gd
+++ entities/bee/bee_manager.gd
@@ -10,48 +10,51 @@ var drawing = false
 
 func _ready():
 	add_child(current_line2D)
-#	last_position = Vector2.ZERO
 
 func _process(delta):
 	if $Path2D/BeeLocator.unit_offset < 1 && started:
 		$Path2D/BeeLocator.offset += speed * delta
 	
 	last_position = $Path2D/BeeLocator.position
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed and event.position.distance_to($Path2D/BeeLocator.position) < 40.0:
-		started = false
-		curve2d = null
-		drawing = true
-		current_line2D.points = PoolVector2Array()
-		current_line2D.show()
 	
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) and drawing:
-		# if this is the first point, add the current position and one just ahead
-		if current_line2D.points.size() == 0:
-			current_line2D.add_point(last_position)
-			var rotation = $Path2D/BeeLocator.rotation
-			current_line2D.add_point(last_position + 10.0 * Vector2(cos(rotation), sin(rotation)))
-		
-		var latest : Vector2 = current_line2D.points[current_line2D.points.size()-1]
-		
-		if event is InputEventMouseMotion and latest.distance_to(event.position) > 10.0:
-			current_line2D.add_point(event.position)
+	if Input.is_action_just_pressed("draw") and get_local_mouse_position().distance_to($Path2D/BeeLocator.global_position) < 40.0:
+		_start_drawing()
+	if Input.is_action_pressed("draw") and drawing:
+		_continue_drawing()
+	if Input.is_action_just_released("draw") and drawing:
+		_finish_drawing()
 	
-	# when mouse is released, switch to the new curve
-	if event is InputEventMouseButton and !event.pressed and drawing:
-		curve2d = Curve2D.new()
-		
-		for i in current_line2D.points.size():
-			curve2d.add_point(current_line2D.points[i])
-		
-		$Path2D.curve = curve2d
-		$Path2D/BeeLocator.offset = 0
-		current_line2D.hide()
-		drawing = false
-		started = true
+	
 
+func _start_drawing():
+	started = false
+	curve2d = null
+	drawing = true
+	current_line2D.points = PoolVector2Array()
+	current_line2D.show()
+
+func _continue_drawing():
+	if current_line2D.points.size() == 0:
+		current_line2D.add_point(last_position)
+		var rotation = $Path2D/BeeLocator.rotation
+		current_line2D.add_point(last_position + 10.0 * Vector2(cos(rotation), sin(rotation)))
+	
+	var latest : Vector2 = current_line2D.points[current_line2D.points.size()-1]
+	
+	if latest.distance_to(get_local_mouse_position()) > 10.0:
+		current_line2D.add_point(get_local_mouse_position())
+
+func _finish_drawing():
+	curve2d = Curve2D.new()
+		
+	for i in current_line2D.points.size():
+		curve2d.add_point(current_line2D.points[i])
+	
+	$Path2D.curve = curve2d
+	$Path2D/BeeLocator.offset = 0
+	current_line2D.hide()
+	drawing = false
+	started = true
 
 func _on_BeeCollisionArea_area_entered(area):
 	print("ran into other bee")
